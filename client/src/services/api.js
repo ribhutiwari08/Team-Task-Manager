5f1d37523b1c816a4b5bd13a21c4375980fe5c1a
Ribhu Bhushan Tiwari <ribhu55555@gmail.com>
2026-05-02 04:07:36 +0530

    fix api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "team-task-manager-production-8806.up.railway.app"
+  baseURL: "https://team-task-manager-production-8806.up.railway.app"
 });
 
 export default API;
