c26c93951f9ad1f9b503eab66a7efab3420818ee
Ribhu Bhushan Tiwari <ribhu55555@gmail.com>
2026-05-02 12:19:31 +0530

    fix env variable

--- server/index.js
+++ server/index.js
@@ -11,9 +11,13 @@ const app = express();
 
 // ✅ CORS FIX (IMPORTANT)
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: [
+    'https://team-task-manager-rosy-ten.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.options("*", cors()); // 🔥 preflight fix
