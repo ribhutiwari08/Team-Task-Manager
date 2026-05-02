81dc39eb70fbfeb97a75d8690fb7e33f55f1297b
Ribhu Bhushan Tiwari <ribhu55555@gmail.com>
2026-05-02 12:12:57 +0530

    fix env variable

--- server/index.js
+++ server/index.js
@@ -1,35 +1,32 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-import cors from "cors";
-import authRoutes from "./routes/auth.js";
 
-app.use("/api/auth", authRoutes);
-app.use(cors({
-  origin: "*"
-}));
+const authRoutes = require("./routes/auth");
+const projectRoutes = require("./routes/project");
+const taskRoutes = require("./routes/task");
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+app.use(cors());   // (for now allow all)
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/task", taskRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("DB connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/project", require("./routes/project"));
-app.use("/api/task", require("./routes/task"));
-
-require("dotenv").config();
-const connectDB = require("./config/db");
-
-connectDB();
